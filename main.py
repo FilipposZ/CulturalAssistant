720184f2557b8267f05c07994b1e5242a7a2d754
ApO <apohofman@gmail.com>
2023-06-17 06:05:25 +0300

    generate a response from openai using user's input

--- main.py
+++ main.py
@@ -6,28 +6,42 @@ import pyttsx3
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+#Modeil list
+# print(openai.Model.list())
+model_id = 'gpt-3.5-turbo'
+
+#gTTS engine
 tts_engine = pyttsx3.init()
-greetings = [
-  "Haaaa you suckuaaa",
-  "Greetings, my name is Hera, i'm an enthousiast about Culture and History, how may i assist you?"
-]
+greetings = "Greetings, my name is Iris, i'm an enthousiast about Culture and History, how may i assist you?"
+
+
+#ChatGpt_Conversation
+def generate_response(prompt_text):
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a cultural information assistant."},
+      {"role": "user", "content": prompt_text}
+    ],
+    max_tokens=691
+  )
+  print(completion.choices[0].message)
+  return completion.choices[0].message
+
+    
+
 
 #openai response
-def openai(text): 
-   def generate_response(prompt):
-      response = openai.Completion.create(
-        engine='text-ada-001',
-        prompt=text,
-        max_tokens=691,
-        temperature=1,
-        n=1,
-        stop=None,
-        log_level='info',
-        logprobs=None,
-        echo=True
-    )
-      return response['choices'][0]['text']
-   return generate_response(text)
+# def openai(text): 
+#    def generate_response(prompt_text):
+#       response = openai.Completion.create(
+#         engine='text-davinci-003',
+#         prompt=messages,
+#         echo=True
+#     )
+#       return response['choices'][0]['text']
+#    return generate_response(text)
+
 
 # Response audio
 def speak_text(text):
@@ -36,25 +50,21 @@ def speak_text(text):
     tts_engine.say(text)
     tts_engine.runAndWait()
 
-def choose_random_greeting():
-  random_indx = random.randint(0, len(greetings))
-  return greetings[random_indx]
 
 # Main
 def main():
     while True:
         #Introduce
-        greeting = choose_random_greeting()
-        print(greeting)
-        speak_text(greeting)
+        print(greetings)
+        speak_text(greetings)
         
         #User input
         user_input = input("Εισάγετε το κείμενο σας: ")
 
         #Openai response
-        response = openai.Completion.create
+        response = generate_response(user_input)
         print(response)
         speak_text(response)
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
   main()
